aed5a84e78886d221ec48a20f131d8ce3246cef3
Mariane McGrath <marianemcgrath@gmail.com>
2025-10-15 22:58:26 +0100

    assessmenttasks_brief

---
+++ assignment/assignment02_bankholidays.py
@@ -0,0 +1,12 @@
+# The program should print out the dates of the bank holidays that happen in northern Ireland.
+
+
+
+
+# Source: https://www.geeksforgeeks.org/python/python-holidays-library/
+
+
+# Modified program that print the bank holidays that are unique to Northern Ireland
+# (i.e. do not happen elsewhere in the UK) you can choose if you want to use the name or 
+# the date of the holiday to decide if it is unique.
+
