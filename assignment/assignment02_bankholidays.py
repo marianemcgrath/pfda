1c911216176f10406d720828e7a7eb90d4c74981
Mariane McGrath <marianemcgrath@gmail.com>
2025-11-27 23:07:12 +0000

    Edits in all Tasks_restart T5_P1

--- assignment/assignment02_bankholidays.py
+++ assignment/assignment02_bankholidays.py
@@ -22,28 +22,25 @@ for event in data ["northern-ireland"] ["events"]:
 
 # Source: PFDA3.1 Class Video (5min 02sec)
 # Source: https://www.geeksforgeeks.org/python/python-holidays-library/
-# Soiurce: https://stackoverflow.com/questions/54987115/uk-bank-holidays-json-read-into-pandas
+# Source: https://stackoverflow.com/questions/54987115/uk-bank-holidays-json-read-into-pandas
 
 
-## Part 2: Write a modified program that print the bank holidays that are unique to Northern Ireland
-# (i.e. do not happen elsewhere in the UK) you can choose if you want to use the name or 
-# the date of the holiday to decide if it is unique.
+## Part 2: A modified program that prints the bank holidays that are unique to Northern Ireland
 
-
-# Extract event titles for each region
+# First, we'll extract event titles for each region
 ni_events = data["northern-ireland"]["events"]
 england_events = data["england-and-wales"]["events"]
 scotland_events = data["scotland"]["events"]
 
-# We'll create sets of holiday titles for England/Wales and Scotland for fast comparison
+# Then, we'll create sets of holiday titles for England/Wales and Scotland
 england_titles = {event["title"] for event in england_events}
 scotland_titles = {event["title"] for event in scotland_events}
 
-# Check each Northern Ireland holiday
+# Finally,we'll check each Northern Ireland holiday
 for event in ni_events:
     holiday_title = event["title"]
 
-    # Check if the title is NOT found in both England/Wales and Scotland sets
+    # And we'll makesure that the title is NOT found in England/Wales and Scotland sets
     if holiday_title not in england_titles and holiday_title not in scotland_titles:
         print(f"Exclusive to NI: {holiday_title} on {event['date']}")
 
